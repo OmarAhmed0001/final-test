a696f153465c34b774e5bcddc2e7be04ce39d04a
Omar Ahmed <75138543+OmarAhmed0001@users.noreply.github.com>
2022-09-19 17:54:35 +0200

    first commit

---
+++ udagram/set_env.sh
@@ -0,0 +1,13 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=postgres
+export POSTGRES_HOST=database-2.cm5e4f3emg7m.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=arn:aws:s3:::randomomar
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=mysecretstring
+export URL=http://Udagram-env.eba-vi2ue2kg.us-east-1.elasticbeanstalk.com
+export Access_key_ID=AKIAUBFDVF4Z7K64COVV
+export Secret_access_key=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
