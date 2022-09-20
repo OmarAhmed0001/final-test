626c8a24067b17f22750496a9ce0287f73940785
Omar Ahmed <75138543+OmarAhmed0001@users.noreply.github.com>
2022-09-20 10:01:28 +0200

    first commit

--- udagram/udagram-api/src/sequelize.ts
+++ udagram/udagram-api/src/sequelize.ts
@@ -1,15 +1,15 @@
 import { Sequelize } from "sequelize-typescript";
 import { config } from "./config/config";
 
-// export const sequelize = new Sequelize({
-//   username: config.username,
-//   password: config.password,
-//   database: config.database,
-//   host: config.host,
-//   port:5432,
-//   dialect: "postgres",
-//   storage: ":memory:",
-// });
-export const sequelize = new Sequelize(
-  `postgres://${config.username}:${config.password}@${config.host}:5432/${config.database}`
-);
+export const sequelize = new Sequelize({
+  username: config.username,
+  password: config.password,
+  database: config.database,
+  host: config.host,
+  port:5432,
+  dialect: "postgres",
+  storage: ":memory:",
+});
+// export const sequelize = new Sequelize(
+//   `postgres://${config.username}:${config.password}@${config.host}:5432/${config.database}`
+// );
